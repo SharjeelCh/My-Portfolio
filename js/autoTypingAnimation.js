00731751b85f59fce794742301515e28c908f350
SharjeelCh <sharjeelh6451@gmail.com>
2024-02-29 12:32:05 +0500

    first

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>As an Android developer specializing in React Native, I thrive on creating dynamic mobile applications that seamlessly integrate with Firebase for robust backend solutions. Leveraging Redux for state management and Node.js for scalable backend development."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
